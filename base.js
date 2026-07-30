dd07067812b1b73ac8af52c41b61803bbd94b161
Mejdz0r <kacper.majorkiewicz@gmail.com>
2026-07-30 17:58:36 +0200

    Enhance calendar with month switching and display
    Refactor calendar functionality to include month switching and display updates.

--- base.js
+++ base.js
@@ -1,17 +1,28 @@
 const nextMontButton = document.getElementById("nextMonth");
 const prevMonthButton = document.getElementById("prevMonth");
-const date = document.getElementById("monthYear");
-const prevMonth = () => {};
-const nextMonth = () => {};
-const creatingCalendarBox = () => {
-    const now = new Date();
+const dateDisplay = document.getElementById("dateDisplay");
+const chosenMonth = document.getElementById("chosenMonth");
+ const now = new Date();
     const currentDate = now.getDate();
     const currentMonth = now.getMonth();
+    let displayMonth = currentMonth;
     const currentYear = now.getFullYear();
     const hour = now.getHours();
     const minute = now.getMinutes();
     const second = now.getSeconds();
-    date.textContent = `${currentDate}/${currentMonth + 1}/${currentYear} ${hour}:${minute}:${second}`;
+const prevMonth = () => {
+    displayMonth--; 
+    switchingMonth();
+    
+};
+const nextMonth = () => {
+   displayMonth++; 
+  switchingMonth();
+    
+};
+const creatingCalendarBox = () => {
+   
+    dateDisplay.textContent = `${currentDate}/${currentMonth + 1}/${currentYear} ${hour}:${minute}:${second}`;
 
 };
 updateTime = () => {
@@ -19,7 +30,49 @@ updateTime = () => {
         creatingCalendarBox();
     }, 1000);
 };
+const switchingMonth = () => {
+    switch (displayMonth) {
+        case 0:
+            chosenMonth.textContent = "January";
+            break;
+        case 1:
+            chosenMonth.textContent = "February";
+            break;
+        case 2:
+            chosenMonth.textContent = "March";
+            break;
+        case 3:
+            chosenMonth.textContent = "April";
+            break;
+        case 4:
+            chosenMonth.textContent = "May";
+            break;
+        case 5:
+            chosenMonth.textContent = "June";
+            break;
+        case 6:
+            chosenMonth.textContent = "July";
+            break;
+        case 7:
+            chosenMonth.textContent = "August";
+            break;
+        case 8:
+            chosenMonth.textContent = "September";
+            break;
+        case 9:
+            chosenMonth.textContent = "October";
+            break;
+        case 10:
+            chosenMonth.textContent = "November";
+            break;
+        case 11:
+            chosenMonth.textContent = "December";
+            break;
+    }
+};
 nextMontButton.addEventListener("click", nextMonth);
 prevMonthButton.addEventListener("click", prevMonth);
+
 creatingCalendarBox();
+switchingMonth();
 updateTime();
